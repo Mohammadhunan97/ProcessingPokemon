6022679e74a1360e8e369a9531e6f4fa7ca6960c
mohammadprofessional <mohammadhunan@gmail.com>
2018-03-11 23:06:29 -0400

    added bg

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,20 +1,26 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class MainApp extends PApplet {
-    public static void MainApp(String args[]) {
+    private PImage plaingrass;
+
+    public static void main(String args[]){
         PApplet.main("MainApp",args);
     }
-
-    public void settings() {
-        fullScreen();
+    public void settings(){
+       size(720, 480);
+      //  fullScreen();
+        plaingrass = loadImage("assets/plaingrass.png");
     }
-
-    public void setup() {
+    public void setup(){
         background(255);
     }
+    public void draw(){
+        plaingrass.resize(width,height);
+        image(plaingrass,0,0);
 
-    public void draw() {
     }
-    public void reset(){
+    void reset(){
     }
+
 }
