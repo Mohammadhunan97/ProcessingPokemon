f30567a30c02acfbfa1c4456f2e809c7c642491d
mohammadprofessional <mohammadhunan@gmail.com>
2018-03-11 22:22:49 -0400

    init

---
+++ src/MainApp.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class MainApp extends PApplet {
+    public static void MainApp(String args[]) {
+        PApplet.main("MainApp",args);
+    }
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        background(255);
+    }
+
+    public void draw() {
+    }
+    public void reset(){
+    }
+}
