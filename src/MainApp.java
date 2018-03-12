6182ceab1e2a1b1fd50070b90163565d66268467
mohammadprofessional <mohammadhunan@gmail.com>
2018-03-11 23:59:14 -0400

    added attack state

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,18 +1,25 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.util.Random;
+
+import java.util.Random;
 
 public class MainApp extends PApplet {
     private Remote r1;
     private PImage plaingrass;
+    private PImage attackbackground;
     private Character player;
     private int playerX = 0;
     private int playerY = 0;
+    private boolean ATTACK_STATE = true;
+
     public static void main(String args[]){
         PApplet.main("MainApp",args);
     }
     public void settings(){
        size(720, 480);
         plaingrass = loadImage("assets/plaingrass.png");
+        attackbackground = loadImage("assets/attackbackground.png");
     }
     public void setup(){
         background(255);
@@ -22,10 +29,17 @@ public class MainApp extends PApplet {
         player = new Character(this);
     }
     public void draw(){
-        handleMovements();
-        plaingrass.resize(width,height);
-        image(plaingrass,0,0);
-        player.loadCharacter(playerX,playerY);
+
+        if(ATTACK_STATE == false){
+            handleMovements();
+            plaingrass.resize(width,height);
+            image(plaingrass,0,0);
+            player.loadCharacter(playerX,playerY);
+        }else{
+            attackbackground.resize(width,height);
+            image(attackbackground,0,0);
+        }
+
     }
     private void reset(){
         playerX = 0;
@@ -37,16 +51,30 @@ public class MainApp extends PApplet {
             if (keyCode == 38) {
                 player.playerMovesUp();
                 playerY -= 2;
+                ATTACK_STATE = isUnderAttack();
             } else if (keyCode == 40) {
                 player.playerMovesDown();
-                playerY += 2;
+                playerY += 4;
+                ATTACK_STATE = isUnderAttack();
             }else if(keyCode == 39){
                 player.playerMovesRight();
-                playerX += 2;
+                playerX += 4;
+                ATTACK_STATE = isUnderAttack();
             }else if(keyCode == 37){
                 player.playerMovesLeft();
-                playerX -= 2;
+                playerX -= 4;
+                ATTACK_STATE = isUnderAttack();
             }
+        }
+    }
+
+    public boolean isUnderAttack(){
+        Random rand = new Random();
+        int  n = rand.nextInt(50) + 1; // 1 to 100 inclusive
+        if( n == 5){
+            return true;
+        }else{
+            return false;
         }
     }
 
